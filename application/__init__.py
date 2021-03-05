2e09b0c180ae594e921a68fe04f61524ced1952a
TrippleCCC <chukwurahnonso@gmail.com>
2021-03-04 23:40:25 -0600

    Add db.py and base template

--- application/__init__.py
+++ application/__init__.py
@@ -1,11 +1,25 @@
+import os
 from flask import Flask
+from flask import render_template
 
 
 def create_app():
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	@app.route('/')
-	def hello():
-		return 'Hello, World!'
-	
-	return app	
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'index.db')
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except:
+        pass
+
+    @app.route('/')
+    def hello():
+        print('Hello, World!')
+        return render_template('base.html')
+    
+    return app	
