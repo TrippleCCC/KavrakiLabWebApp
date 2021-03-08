a495cef42915e10dbc5d5c94e13dfd92ba4086a1
TrippleCCC <chukwurahnonso@gmail.com>
2021-03-08 13:46:33 -0600

    Fix any-allele any-pepetide bug in home.py; Change protein->peptide

--- application/home.py
+++ application/home.py
@@ -14,36 +14,37 @@ def home():
 @bp.route("/search", methods=["POST"])
 def search():
     allele = request.form["allele"]
-    protein = request.form["protein"]
+    peptide = request.form["peptide"]
     allele = allele if allele else "any-allele"
-    protein = protein if protein else "any-protein"
-    return redirect(url_for("home.results", allele=allele, protein=protein))
+    peptide = peptide if peptide else "any-peptide"
+    return redirect(url_for("home.results", allele=allele, peptide=peptide))
 
-@bp.route("/results/<allele>/<protein>", methods=["GET"])
-def results(allele, protein):
+@bp.route("/results/<allele>/<peptide>", methods=["GET"])
+def results(allele, peptide):
     db = get_db()
     data = []
 
     start = time.time()
     # Check the parameters and search the database
-    if (allele, protein) == ("any-allele", "any-protein"):
-        return render_template("results.html")
+    # TODO: add pagination functionality for these results.
+    if (allele, peptide) == ("any-allele", "any-peptide"):
+        data = db.execute("SELECT * FROM pdb_files LIMIT 2000").fetchall()
     elif allele == "any-allele":
-        data = db.execute("""SELECT * FROM pdb_files WHERE protein = ?""",
-                (protein,)).fetchall()
-    elif protein == "any-protein":
+        data = db.execute("""SELECT * FROM pdb_files WHERE peptide = ?""",
+                (peptide,)).fetchall()
+    elif peptide == "any-peptide":
         data = db.execute("""SELECT * FROM pdb_files WHERE allele = ?""",
                 (allele,)).fetchall()
     else:
         data = db.execute("""SELECT * FROM pdb_files WHERE allele = ?
-            AND protein = ?""", (allele, protein)).fetchall()
+            AND peptide = ?""", (allele, peptide)).fetchall()
     end = time.time()
 
     query_time = end - start
     num_results = len(data)
 
     return render_template("results.html", results=data, allele=allele, 
-            protein=protein, num_results=num_results, query_time=query_time)
+            peptide=peptide, num_results=num_results, query_time=query_time)
 
 @bp.route("/suggest/<suggest_type>", methods=["GET"])
 def suggest(suggest_type):
@@ -52,9 +53,9 @@ def suggest(suggest_type):
     if suggest_type == 'allele':
         data = db.execute("SELECT DISTINCT allele FROM pdb_files WHERE allele LIKE ? LIMIT 5", (f'%{query}%',)).fetchall()
         data = list(map(lambda r: {"value":r["allele"], "data":r["allele"]}, data))
-    elif suggest_type == "protein":
-        data = db.execute("SELECT DISTINCT protein FROM pdb_files WHERE protein LIKE ? LIMIT 5", (f'%{query}%',)).fetchall()
-        data = list(map(lambda r: {"value":r["protein"], "data":r["protein"]}, data))
+    elif suggest_type == "peptide":
+        data = db.execute("SELECT DISTINCT peptide FROM pdb_files WHERE peptide LIKE ? LIMIT 5", (f'%{query}%',)).fetchall()
+        data = list(map(lambda r: {"value":r["peptide"], "data":r["peptide"]}, data))
     else:
         data = []
 
